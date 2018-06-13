f5153b93f0d2e43ef28ae08768bc020aba2f9131
Antoine Dewaele <antoinee.dewaele@gmail.com>
2018-06-13 16:07:31 +0200

    fix customer transformer

--- app/Transformers/MarketplaceCriteriaTransformer.php
+++ app/Transformers/MarketplaceCriteriaTransformer.php
@@ -1,40 +1,40 @@
 <?php
 
-namespace App\Transformers;
-
-use App\Models\MarketplaceCriteria;
-use App\Models\Marketplace;
-
-class MarketplaceCriteriaTransformer extends BaseTransformer
-{
-    protected $availableIncludes = [
-        'marketplace', 'marketplace_criteria_rating'
-    ];
-
-    /**
-     * Turn this item object into a generic array
-     *
-     * @param MarketplaceCriteria $marketplaceCriteria
-     * @return array
-     */
-    public function transform(MarketplaceCriteria $marketplaceCriteria)
-    {
-        return parent::addMeta([
-            'id' => $marketplaceCriteria->id,
-            'name' => $marketplaceCriteria->name,
-            'weight' => $marketplaceCriteria->weight,
-            'created_at' => $marketplaceCriteria->created_at,
-            'updated_at' => $marketplaceCriteria->updated_at
-        ]);
-    }
-
-    public function includeMarketplace(MarketplaceCriteria $marketplaceCriteria)
-    {
-        return $this->item($marketplaceCriteria->marketplace, new MarketplaceTransformer());
-    }
-
-    public function includeMarketplaceCriteriaRating(MarketplaceCriteria $marketplaceCriteria)
-    {
-        return $this->collection($marketplaceCriteria->marketplace_criteria_ratings, new MarketplaceCriteriaRatingTransformer());
-    }
+//namespace App\Transformers;
+//
+//use App\Models\MarketplaceCriteria;
+//use App\Models\Marketplace;
+//
+//class MarketplaceCriteriaTransformer extends BaseTransformer
+//{
+//    protected $availableIncludes = [
+//        'marketplace', 'marketplace_criteria_rating'
+//    ];
+//
+//    /**
+//     * Turn this item object into a generic array
+//     *
+//     * @param MarketplaceCriteria $marketplaceCriteria
+//     * @return array
+//     */
+//    public function transform(MarketplaceCriteria $marketplaceCriteria)
+//    {
+//        return parent::addMeta([
+//            'id' => $marketplaceCriteria->id,
+//            'name' => $marketplaceCriteria->name,
+//            'weight' => $marketplaceCriteria->weight,
+//            'created_at' => $marketplaceCriteria->created_at,
+//            'updated_at' => $marketplaceCriteria->updated_at
+//        ]);
+//    }
+//
+//    public function includeMarketplace(MarketplaceCriteria $marketplaceCriteria)
+//    {
+//        return $this->item($marketplaceCriteria->marketplace, new MarketplaceTransformer());
+//    }
+//
+//    public function includeMarketplaceCriteriaRating(MarketplaceCriteria $marketplaceCriteria)
+//    {
+//        return $this->collection($marketplaceCriteria->marketplace_criteria_ratings, new MarketplaceCriteriaRatingTransformer());
+//    }
 }
